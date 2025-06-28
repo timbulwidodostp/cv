bc4a6024a0fd83fac4ee039dc284da689f94834e
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-06-29 06:31:07 +0700

    cv With (In) R Software

--- cv/cv.R
+++ cv/cv.R
@@ -6,6 +6,7 @@ install.packages("cv")
 library("cv")
 cv = read.csv("https://raw.githubusercontent.com/timbulwidodostp/cv/main/cv/cv.csv",sep = ";")
 # Estimation Cross-Validate Regression Models Use cv With (In) R Software
+m.auto <- lm(Dependen ~ Independen_1 + Independen_2 + Independen_3, data=cv)
 summary(cv.m.auto <- cv(m.auto))
 summary(cv(m.auto, k= "loo"))
 summary(cv(m.auto, k = "loo", method = "naive"))
